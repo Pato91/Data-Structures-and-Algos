65acd5cf33d800f9c36ad8c477584ec6bf402a61
Pato91 <ayebalebj@gmail.com>
2018-10-03 20:51:01 +0300

    Minor edits

--- insertion-sort/sort.cc
+++ insertion-sort/sort.cc
@@ -73,7 +73,7 @@ int find_element(std::vector<int> input, int target){
 
 /**
  * This is a depth first algorithm and it is efficient when sorting small data sets.
- * It runs in O(n*n) meaning that for a given input of size n,
+ * It runs in O(n*n)<worst case> meaning that for a given input of size n,
  * the running time grows by a factor n indicating that it 
  * be inefficient for large sized data sets.
  * 
