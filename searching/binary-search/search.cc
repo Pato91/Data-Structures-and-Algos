33198d74d9f9f687d10d87992f0c2bf41883b123
Pato91 <ayebalebj@gmail.com>
2018-10-18 08:55:19 +0300

    Adds binary search algo

--- searching/binary-search/search.cc
+++ searching/binary-search/search.cc
@@ -1,11 +1,11 @@
 #include "./search.h"
 
 int main(){
-    std::vector<int> instance(100);
-    std::generate(instance.begin(), instance.end(), []() -> int {return rand() % 100;});
+    std::vector<int> instance(1000000);
+    std::generate(instance.begin(), instance.end(), []() -> int {return rand() % 1000000;});
     merge_sort(instance.begin(), instance.end());
     auto start = std::chrono::high_resolution_clock::now();
-    int res = binary_search(&instance, instance.at(52), instance.size());
+    int res = binary_search(&instance, instance.at(1002), instance.size());
     auto end = std::chrono::high_resolution_clock::now();
     double v = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     printf("%d", res);
@@ -13,3 +13,23 @@ int main(){
     printf("\n\n\nTook %f seconds to execute\n\n\n", v/1000000);
     return 0;
 }
+
+
+/**
+ * This is algorithm uses the devide and conquer strategy(break large problems into small ones
+ * you then recursively search for the target by halving and shifting ranges). It runs in O(log(n))
+ * <worst case> meaning that for a given input of size n, it finds the target in log(n) steps.
+ * It is really efficient. For a list of 16 elements, you'd only need 4 steps ~ log base 2 of 16
+ * ... list of 1,000 ~ 10 steps and list of 1,000,000 ~ 19 steps
+ * 
+ * Prerequisites 
+ * -The list of items has to be sorted before hand
+ * 
+ * A simple bench mark showed that; 
+ *      
+ *     Input size (n) | list order | Running time (in seconds)   
+ *     100            | INCR       | 0.000001                                    
+ *     100,000        | INCR       | 0.000001                            
+ *     200,000        | INCR       | 0.000001                                    
+ *     1,000,000      | INCR       | 0.000002                         
+*/
--- searching/binary-search/search.h
+++ searching/binary-search/search.h
@@ -6,13 +6,13 @@
 
 int binary_search(std::vector<int> * input, int target, int n, int acc = 0){
     if(n >= 1){
-        int midIndex = acc + (n - 1) / 2;
-        printf("here");
+        int midIndex = acc + (n - acc) / 2;
         if(input->at(midIndex) == target)
             return midIndex;
         if(input->at(midIndex) > target)
-            return binary_search(input, target, midIndex - 1, acc);
+            return binary_search(input, target, midIndex - 1);
 
+        
         return binary_search(input, target, n, midIndex + 1);
     }
     return -1;
